4aed0f1a39ccacd8d7e293885e3f2a5c37f0c2ce
contactonosotrascr <contactonosotrascr@gmail.com>
2020-06-07 23:59:55 -0600

    [VP] Deleted plugin 'Increase Maximum Upload File Size'
    VP-Action: plugin/delete/upload-max-file-size/upload-max-file-size.php
    VP-Plugin-Name: Increase Maximum Upload File Size
    
    X-VP-Version: 4.0-beta2
    X-VP-Environment: fran-alpha

--- wp-content/vpdb/.schema/termmeta.sql
+++ wp-content/vpdb/.schema/termmeta.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `<<table-prefix>>termmeta` (
   `meta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `term_id` bigint(20) unsigned NOT NULL DEFAULT '0',
-  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci,
+  `meta_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `meta_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci,
   PRIMARY KEY (`meta_id`),
   KEY `term_id` (`term_id`),
   KEY `meta_key` (`meta_key`(191))
