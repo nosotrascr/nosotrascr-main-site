f0a4730f8d69ea910ad355ea63a47d114eb75d5f
Francisco Gomez <fgomeza25@gmail.com>
2020-06-13 13:54:40 -0600

    Removing versionpress db

--- wp-content/vpdb/.schema/options.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `<<table-prefix>>options` (
-  `option_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `option_name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `option_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `autoload` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'yes',
-  PRIMARY KEY (`option_id`),
-  UNIQUE KEY `option_name` (`option_name`),
-  KEY `autoload` (`autoload`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci
